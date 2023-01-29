7b9224e945c380f86bb4edd28fb34298f37119e3
Sekseli3 <120391401+Sekseli3@users.noreply.github.com>
2023-01-29 09:50:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
+
+//app.use(morgan('tiny'))
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'));
+
 
 app.use(express.json())
 
